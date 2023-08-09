35354e574a0c35865984ac4aab2f2e26820416eb
mattbrailsford <me@mattbrailsford.com>
2023-08-09 15:24:17 +0100

    Update revalidation topic header name

--- lib/umbraco-commerce/index.ts
+++ lib/umbraco-commerce/index.ts
@@ -768,7 +768,7 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
   // otherwise it will continue to retry the request.
   const collectionWebhooks = ['collections/create', 'collections/delete', 'collections/update'];
   const productWebhooks = ['products/create', 'products/delete', 'products/update'];
-  const topic = headers().get('x-shopify-topic') || 'unknown';
+  const topic = headers().get('x-topic') || 'unknown';
   const secret = req.nextUrl.searchParams.get('secret');
   const isCollectionUpdate = collectionWebhooks.includes(topic);
   const isProductUpdate = productWebhooks.includes(topic);
